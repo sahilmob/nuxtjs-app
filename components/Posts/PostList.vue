3180c6ea440a84d5fd7664f554a85a55f1b7d594
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-08-09 18:43:51 +0300

    Move the section that contains PostPreview element to itw own component 'PostList'

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="post-list">
+    <PostPreview id="1" thumbnail="https://cdn.pixabay.com/photo/2015/09/17/17/25/code-944499_1280.jpg" title="Hello" previewText="This is my first post" />
+    <PostPreview id="2" thumbnail="https://cdn.pixabay.com/photo/2015/09/17/17/25/code-944499_1280.jpg" title="Hello there - the second time!" previewText="This is my second post" />
+    <PostPreview id="1" thumbnail="https://cdn.pixabay.com/photo/2015/09/17/17/25/code-944499_1280.jpg" title="Hello there - the third time!" previewText="This is my third post" />
+  </section>
+</template>
+
+<script>
+import PostPreview from "@/components/Posts/PostPreview";
+export default {
+  components: {
+    PostPreview
+  }
+};
+</script>
+
+
+<style scoped>
+.post-list {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
+
--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,19 +1,15 @@
 <template>
   <div class="posts=page">
-    <section class="post-list">
-      <PostPreview id="1" thumbnail="https://cdn.pixabay.com/photo/2015/09/17/17/25/code-944499_1280.jpg" title="Hello" previewText="This is my first post" />
-      <PostPreview id="2" thumbnail="https://cdn.pixabay.com/photo/2015/09/17/17/25/code-944499_1280.jpg" title="Hello there - the second time!" previewText="This is my second post" />
-      <PostPreview id="1" thumbnail="https://cdn.pixabay.com/photo/2015/09/17/17/25/code-944499_1280.jpg" title="Hello there - the third time!" previewText="This is my third post" />
-    </section>
+    <PostList />
   </div>
 </template>
 
 <script>
-import PostPreview from "@/components/Posts/PostPreview";
+import PostList from "@/components/Posts/PostList";
 
 export default {
   components: {
-    PostPreview
+    PostList
   }
 };
 </script>
@@ -24,14 +20,5 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
-}
-
-.post-list {
-  display: flex;
-  padding: 20px;
-  box-sizing: border-box;
-  flex-wrap: wrap;
-  align-items: center;
-  justify-content: center;
 }
 </style>
