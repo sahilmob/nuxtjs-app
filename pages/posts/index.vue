dd709f93ba4abfb04209af3b81fc7f03f78d5248
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-08-09 18:35:25 +0300

    Add html structure and style to the posts index file

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,8 +1,22 @@
 <template>
-  <div class="posts-page">
-
+  <div class="posts=page">
+    <section class="post-list">
+      <PostPreview id="1" thumbnail="https://cdn.pixabay.com/photo/2015/09/17/17/25/code-944499_1280.jpg" title="Hello" previewText="This is my first post" />
+      <PostPreview id="2" thumbnail="https://cdn.pixabay.com/photo/2015/09/17/17/25/code-944499_1280.jpg" title="Hello there - the second time!" previewText="This is my second post" />
+      <PostPreview id="1" thumbnail="https://cdn.pixabay.com/photo/2015/09/17/17/25/code-944499_1280.jpg" title="Hello there - the third time!" previewText="This is my third post" />
+    </section>
   </div>
 </template>
+
+<script>
+import PostPreview from "@/components/Posts/PostPreview";
+
+export default {
+  components: {
+    PostPreview
+  }
+};
+</script>
 
 
 <style scoped>
@@ -10,5 +24,14 @@
   display: flex;
   justify-content: center;
   align-items: center;
+}
+
+.post-list {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
 }
 </style>
