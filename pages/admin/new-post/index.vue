c28571c68b5f4749cb7369e1af8df0c0b7d4577c
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-08-18 22:31:50 +0300

    Add logic to post data to the database using axios

--- pages/admin/new-post/index.vue
+++ pages/admin/new-post/index.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="admin-new-post-page">
     <section class="new-post-form">
-      <admin-post-form></admin-post-form>
+      <admin-post-form @submit="onSubmitted"></admin-post-form>
     </section>
   </div>
 </template>
 
 <script>
 import AdminPostForm from "@/components/Admin/AdminPostForm";
-
+import axios from "axios";
 export default {
   layout: "admin",
   components: {
     AdminPostForm
+  },
+  methods: {
+    onSubmitted(postData) {
+      axios
+        .post("https://nuxt-blog-ab0d8.firebaseio.com/posts.json", postData)
+        .then(res => {
+          console.log(res);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    }
   }
 };
 </script>
