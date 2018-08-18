519acdf4da048098bb69fb54544307bb036bd221
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-08-18 18:58:25 +0300

    Add admin layout and error layout

---
+++ layouts/admin.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+
+        <nuxt/>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style>
+html {
+  font-family: "Open sans", "sans-serif";
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ layouts/error.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="error-page">
+        <h1>Oops, something went wrong!</h1>
+        <!-- used normal link to be safe if nuxt craches on error -->
+        <p>Back to
+            <a href="/">safety</a>!</p>
+    </div>
+</template>
+
+<style scoped>
+.error-page {
+  text-align: center;
+}
+
+.error-page a {
+  text-decoration: none;
+  color: red;
+}
+
+.error-page a:hover,
+.error-page a:active {
+  color: salmon;
+}
+</style>
