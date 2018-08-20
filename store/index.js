ed27a6ef961defeed4bf6e7425071d203d30f1cb
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-08-20 22:57:42 +0300

    move local storage clearing logic inside check if code is running on client

--- store/index.js
+++ store/index.js
@@ -129,8 +129,10 @@ const createStore = () => {
                 vuexContext.commit('clearToken');
                 Cookie.remove('jwt');
                 Cookie.remove('expirationDate');
-                localStorage.removeItem('token');
-                localStorage.removeItem('tokenExpiration');
+                if (process.client) {
+                    localStorage.removeItem('token');
+                    localStorage.removeItem('tokenExpiration');
+                }
             }
         },
         getters: {
